48466e753e3a6fb7e5e6c1a119343eeabf0a872a
hurcy <cinyoung.hur@gmail.com>
2025-03-12 22:07:35 +0900

    fix: sdk version and upload test crt

--- git-proxy/scripts/upload.sh
+++ git-proxy/scripts/upload.sh
@@ -3,3 +3,4 @@ databricks catalogs create main --storage-root s3://hurcy-rootbucket/main
 databricks schemas create default main
 databricks volumes create main default scripts MANAGED
 databricks fs cp init_scm_ca.sh dbfs:/Volumes/main/default/scripts/
+databricks fs cp scm-ca.crt dbfs:/Volumes/main/default/scripts/
