c6cc9150eac2f55f2b72a3db4cac21326eb3e313
hurcy <cinyoung.hur@gmail.com>
2025-03-31 22:14:27 +0900

    feat: create catalog, schema, volume

--- presets_with_schema/scratch/to_yaml.py
+++ presets_with_schema/scratch/to_yaml.py
@@ -1,8 +1,8 @@
 import csv
 import yaml
 
-def csv_to_yaml(csv_path, yaml_path):
-    resources = {"resources": {"schemas": {}, "volumes": {}}}
+def create_schemas(csv_path, yaml_path):
+    resources = {"resources": {"schemas": {}}}
     
     with open(csv_path, "r") as csv_file:
         reader = csv.DictReader(csv_file)
@@ -14,6 +14,18 @@ def csv_to_yaml(csv_path, yaml_path):
                 "name": system_key,
                 "comment": row["description"]
             }
+    
+    with open(yaml_path, "w") as yaml_file:
+        yaml.dump(resources, yaml_file, default_flow_style=False, sort_keys=False)
+
+def create_volumes(csv_path, yaml_path):
+    resources = {"resources": {"volumes": {}}}
+    
+    with open(csv_path, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            # Schema 생성
+            system_key = row["system"]
             
             # Volume 생성
             volume_key = row["transfer_volume"]
@@ -27,4 +39,5 @@ def csv_to_yaml(csv_path, yaml_path):
         yaml.dump(resources, yaml_file, default_flow_style=False, sort_keys=False)
 
 # 사용 예시
-csv_to_yaml("input.csv", "../resources/multi_schema.yaml")
+create_schemas("input.csv", "../resources/schema.yml")
+create_volumes("input.csv", "../resources/volume.yml")
