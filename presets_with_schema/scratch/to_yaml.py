390f7e7671dfeec8522b1f5c9fe938c025240430
hurcy <cinyoung.hur@gmail.com>
2025-04-01 20:03:27 +0900

    feat: merge create catalog and schema function into one module

--- presets_with_schema/scratch/to_yaml.py
+++ presets_with_schema/scratch/to_yaml.py
@@ -1,5 +1,29 @@
 import csv
 import yaml
+import subprocess
+import pandas as pd
+
+def create_catalog(csv_path):
+    cols = ['company','business_unit']
+    df = pd.read_csv(csv_path)[cols].drop_duplicates()
+    for _, row in df.iterrows():
+        catalog_name = f"{row.company}_{row.business_unit}_mig"
+        try:
+            commands = [
+                    "databricks",
+                    "catalogs",
+                    "create",
+                    catalog_name,
+                    "--comment",
+                    catalog_name,
+                    "--storage-root",
+                    f"s3://hurcy-rootbucket/{catalog_name}",
+            ]
+            return subprocess.run(
+                commands, capture_output=True, text=True
+            ).stdout.strip("\n")
+        except subprocess.CalledProcessError as e:
+            raise Exception(f"FAILED: {e.output.decode()}")
 
 def create_schemas(csv_path, yaml_path):
     resources = {"resources": {"schemas": {}}}
@@ -39,5 +63,6 @@ def create_volumes(csv_path, yaml_path):
         yaml.dump(resources, yaml_file, default_flow_style=False, sort_keys=False)
 
 # 사용 예시
+create_catalog("input.csv")
 create_schemas("input.csv", "../resources/schema.yml")
 create_volumes("input.csv", "../resources/volume.yml")
